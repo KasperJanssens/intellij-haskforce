33843558986eec947113c18dd3c04cc760989d30
Kasper <kasper.janssens@amplidata.com>
2015-03-18 09:57:34 +0000

    Before this test can ever work, we need to get the cabal stuff in this branch.

--- tests/com/haskforce/move/HaskellSimpleMoveTest.java
+++ tests/com/haskforce/move/HaskellSimpleMoveTest.java
@@ -15,11 +15,12 @@ public class HaskellSimpleMoveTest extends HaskellLightPlatformCodeInsightFixtur
     }
 
     public void testSimpleMove(){
-        PsiFile[] files = myFixture.configureByFiles("From/MoveMe.hs", "To/Token.hs");
+        PsiFile[] files = myFixture.configureByFiles("From/MoveMe.hs", "To/Token.hs", "SimpleMove.cabal");
         myFixture.moveFile("From/MoveMe.hs", "To/");
 
         myFixture.checkResultByFile("To/Token.hs", "To/Token-after.hs", false);
         myFixture.checkResultByFile("To/MoveMe.hs", "From/MoveMe-after.hs", false);
+        myFixture.checkResultByFile("SimpleMove.cabal", "SimpleMove-after.cabal", false);
     }
     
 
